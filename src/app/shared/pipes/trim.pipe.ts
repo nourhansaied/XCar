a665b39e5de35f5247f44b9b95ea9ab63ce0f0ec
Mohamed Essam <sahar.kassem92@gmail.com>
2020-03-08 22:37:23 +0200

    change

--- src/app/shared/pipes/trim.pipe.ts
+++ src/app/shared/pipes/trim.pipe.ts
@@ -3,7 +3,7 @@ import { Pipe, PipeTransform } from "@angular/core";
 @Pipe({
   name: "trim"
 })
-export class TrimPipe implements PipeTransform {
+export class TrimPipe implements PipeTransform { 
   transform(value: any, args?: any): any {
     if (value) return value.toString().substring(0, args - 3);
   }
