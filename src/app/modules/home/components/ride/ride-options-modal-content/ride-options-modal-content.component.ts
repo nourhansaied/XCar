e473a3aa0cc85a12d4451d3ea706fca8b8182610
Sahar Kassem <sahar.kassem@linkdev.com>
2020-04-02 23:23:02 +0200

    register forms

---
+++ src/app/modules/home/components/ride/ride-options-modal-content/ride-options-modal-content.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { SharedService } from 'src/app/shared/services/shared.service';
+import { Constant } from 'src/app/shared/constant';
+
+@Component({
+  selector: 'victoria-ride-options-modal-content',
+  templateUrl: './ride-options-modal-content.component.html',
+  styleUrls: ['./ride-options-modal-content.component.scss']
+})
+export class RideOptionsModalContentComponent implements OnInit {
+  
+  selectedRide: string;
+  rideOptions: object;
+  @Input() title: string;
+
+  constructor(private sharedSer: SharedService) {}
+  ngOnInit() {
+    this.rideOptions = Constant.rideOptions;
+    this.sharedSer
+      .getRideSelection()
+      .subscribe(option => (this.selectedRide = option));
+  }
+
+  public updateRideSelection(rideOption) {
+    this.sharedSer.setRideSelection(rideOption);
+  }
+
+}
